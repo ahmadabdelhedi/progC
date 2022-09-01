1e2351c26a111f5482a52268683cc98bab5440df
ahmadinfo <ahmad.abdelhedi15@gmail.com>
2022-09-01 18:05:33 +0100

    recherche chaine

---
+++ recherche_chaine.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+        int i,j;
+        char ch[100];
+        printf("donner la chaine ch :\n");
+        gets(ch);
+
+        char ch1[10];
+        printf("donner la chaine ch1:\n");
+        gets(ch1);
+          i=0;
+         while(i<strlen(ch)-strlen(ch1))
+         {
+              j=0;
+             while((j<strlen(ch1))&&(ch[i+j]==ch1[j]))
+                j++;
+
+             if(j==strlen(ch1))
+             {
+                printf("%s existe dans %s",ch1,ch);
+                break;
+             }
+
+           i++;
+
+         }
+         if(i==strlen(ch)-strlen(ch1))
+            printf("%s n'existe pas dans %s",ch1,ch);
+
+
+
+
+
+    return 0;
+}
